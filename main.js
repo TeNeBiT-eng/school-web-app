19602124ec57a09356367f2d6e1e8ad96513c804
Tomisin Babalola <ademolababalola2003@gmail.com>
2022-08-29 12:15:23 +0100

    mediaquery for homepage

--- main.js
+++ main.js
@@ -15,10 +15,30 @@ faqs.forEach((faq) => {
 
     // change icon on show
     const icon = faq.querySelector(".faq_icon i");
-    if ((icon.className === "uil uil-plus")) {
+    if (icon.className === "uil uil-plus") {
       icon.className = "uil uil-minus";
     } else {
       icon.className = "uil uil-plus";
     }
   });
 });
+
+// Show/hide nav menu
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
+
+// close nav menu
+const closeNav = () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
